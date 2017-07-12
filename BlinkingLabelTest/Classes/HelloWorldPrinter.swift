d88368a399a20667845504fc0487f67222eff775
Ihor Myroniuk <ihor.myroniuk@brander.ua>
2017-07-12 14:47:14 +0300

    Bla bla

---
+++ BlinkingLabelTest/Classes/HelloWorldPrinter.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldPrinter.swift
+//  Pods
+//
+//  Created by Ihor Myroniuk on 7/12/17.
+//
+//
+
+import Foundation
+
+class HelloWorldPrinter {
+    
+    static func printHelloWorld() {
+        print("Hello, World!!! From pod")
+    }
+    
+}
